ba3acce5eb7db486dd308469b14a01b8f0e11049
Your Name <you@example.com>
2021-04-17 11:20:16 +0900

    Fix

--- request-generate-api-client.sh
+++ request-generate-api-client.sh
@@ -2,7 +2,7 @@
 
 cd ~/project
 
-readonly changed_schema=`git diff HEAD^ HEAD --name-only swagger.yml`
+readonly changed_schema=`git diff HEAD^ --name-only swagger.yml`
 
 if [ "${changed_schema}" != "" ]; then
   curl -u ${OPEN_API_CLIENT2_TOKEN}: -d build_parameters[CIRCLE_JOB]=create-api-client-pr \
