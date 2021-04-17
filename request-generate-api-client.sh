253ae4a0f9c1590e41803d59ffd2b35ab28914cf
emgniddikur <yoshiharu2580@gmail.com>
2021-04-17 10:13:55 +0900

    Fix

--- request-generate-api-client.sh
+++ request-generate-api-client.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+cd ~/project
+
 readonly changed_schema=`git diff HEAD^ HEAD --name-only swagger.yml`
 
 if [ "${changed_schema_definition_file}" != "" ]; then
-  curl -u ${OPEN_API_CLIENT_TOKEN}: -d build_parameters[CIRCLE_JOB]=create-api-client-pr https://circleci.com/api/v1.1/project/github/emgniddikur/OpenAPI-client2/tree/develop
+  curl -u ${OPEN_API_CLIENT2_TOKEN}: -d build_parameters[CIRCLE_JOB]=create-api-client-pr \
+  https://circleci.com/api/v1.1/project/github/emgniddikur/OpenAPI-client2/tree/develop
 fi
